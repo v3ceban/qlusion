abd3077543d6cc6b0e508a2d05652d36a9555732
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-06-03 16:24:00 -0700

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { AppProvider } from "./Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <AppProvider>
       <Header />
       <Main />
+      <Analytics />
     </AppProvider>
   );
 }

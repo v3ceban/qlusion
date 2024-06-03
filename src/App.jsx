2f5055c088fc3646b39bac3eea06b791f633d682
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-06-03 16:43:08 -0700

    vercel speedinsights

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import { AppProvider } from "./Providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
-    <AppProvider>
-      <Header />
-      <Main />
+    <>
+      {/* App */}
+      <AppProvider>
+        <Header />
+        <Main />
+      </AppProvider>
+
+      {/* Analytics */}
       <Analytics />
-    </AppProvider>
+      <SpeedInsights />
+    </>
   );
 }

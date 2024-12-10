55e6ce975f504b1b72d7033daebaa24b359ae811
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-12-09 18:49:32 -0800

    styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,10 @@ export const metadata = {
   title: "Qlusion",
   description:
     "Qlusion: the scheduling solution for SCU clubs, student organizations, and more.",
+  meta: {
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1",
+  },
 };
 
 export default async function RootLayout({ children }) {
